6c8447ae51f2ff90188528c61986b5035484d743
Ha Ngo <93944986+havngo@users.noreply.github.com>
2025-03-27 12:53:47 +0700

    attempt to fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(cors({ origin: process.env.CLIENT_URL }));
+app.options('*', cors())
 app.use('/', routes);
 
 app.listen(port, () => {
